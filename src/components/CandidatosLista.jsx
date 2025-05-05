c75704dab21a97401e5370ebf1b385ee6192ff97
Fernando Gaitán <fernandoggaitan@gmail.com>
2025-05-05 08:51:24 -0300

    Update CandidatosLista.jsx

--- src/components/CandidatosLista.jsx
+++ src/components/CandidatosLista.jsx
@@ -1,52 +1,30 @@
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import CandidatoItem from './CandidatoItem';
-import { useState } from 'react';
 
 export default function CandidatosLista(){
 
-    const [cant_votos, setCantVotos] = useState(0);
-
-    const [lista, setLista] = useState([
-        {
-            ID: 1,
-            nombre: "Sofía",
-            imagen: "https://randomuser.me/api/portraits/women/90.jpg",
-            votos: 0
-        },
-        {
-            ID: 2,
-            nombre: "Pablo",
-            imagen: "https://randomuser.me/api/portraits/men/49.jpg",
-            votos: 0
-        },
-        {
-            ID: 3,
-            nombre: "Valeria",
-            imagen: "https://randomuser.me/api/portraits/women/61.jpg",
-            votos: 0
-        }
-    ]);
-
-    const handlerCandidato = (diferencia) => {
-        setCantVotos( cant_votos + diferencia );
-    }
-
     return (
         <>
-            <p> Cantidad: {cant_votos} </p>
             <Row>
-                {lista.map( c => (
-                    <Col key={c.ID}>
-                        <CandidatoItem
-                            ID={c.ID}           
-                            nombre={c.nombre}
-                            imagen={c.imagen}
-                            initialVotos={c.votos}
-                            changeVotos={handlerCandidato}
-                        />
-                    </Col>
-                ))}
+                <Col>
+                    <CandidatoItem 
+                        nombre="Sofía"
+                        imagen="https://randomuser.me/api/portraits/women/90.jpg"
+                    />
+                </Col>
+                <Col>
+                    <CandidatoItem 
+                        nombre="Pablo"
+                        imagen="https://randomuser.me/api/portraits/men/49.jpg"
+                    />
+                </Col>
+                <Col>
+                    <CandidatoItem 
+                        nombre="Valeria"
+                        imagen="https://randomuser.me/api/portraits/women/61.jpg"
+                    />
+                </Col>
             </Row>
         </>
     );
